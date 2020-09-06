954bb90fd904ec363e83d3b2676d1a61f95db842
Alex Khomenko <sasvsha@gmail.com>
2020-09-06 18:44:50 +0200

    Updates

---
+++ precache-manifest.ca7165ba70dadd283cfc46bea514cfc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9b4a5b3dbe9d3146ca64e59b502612",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "58602ae389d591b9c5b0",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.a908e0f2.chunk.css"
+  },
+  {
+    "revision": "41dccbe27695f35ae26f",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.71703519.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.71703519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58602ae389d591b9c5b0",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.fad76e6b.chunk.js"
+  },
+  {
+    "revision": "55581d768912a9cdd1d4",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.3a4847fc.js"
+  }
+]);
