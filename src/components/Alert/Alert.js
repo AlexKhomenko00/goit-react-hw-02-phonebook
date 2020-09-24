34fef65041a0702a6b0e6ba525675508f5d5f778
Alex Khomenko <sasvsha@gmail.com>
2020-09-24 23:03:51 +0200

    added homework 04

---
+++ src/components/Alert/Alert.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import s from './Alert.module.css';
+
+const Alert = () => {
+  return <div className={s.alert}>Contact already exists!</div>;
+};
+
+export default Alert;
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import s from './ContactList.module.css';
+import fadeContact from './fade.module.css';
 
 const ContactList = ({ contacts, onRemoveContact }) => {
   return (
-    <ul className={s.contacts}>
+    <TransitionGroup component="ul" className={s.contacts}>
       {contacts.map(({ id, name, number }) => (
-        <li className={s.contact} key={id}>
-          <p>
-            <span> {name}:</span> <span>{number}</span>
-          </p>
-          <section>
-            <button type="button" onClick={() => onRemoveContact(id)}>
-              Удалить
-            </button>
-          </section>
-        </li>
+        <CSSTransition
+          key={id}
+          classNames={fadeContact}
+          timeout={250}
+          appear={true}
+        >
+          <li className={s.contact}>
+            <p>
+              <span> {name}:</span> <span>{number}</span>
+            </p>
+            <div>
+              <button type="button" onClick={() => onRemoveContact(id)}>
+                Delete
+              </button>
+            </div>
+          </li>
+        </CSSTransition>
       ))}
-    </ul>
+    </TransitionGroup>
   );
 };
 
